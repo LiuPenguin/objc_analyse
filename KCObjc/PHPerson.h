162a026166ea856a94e07271e899a1309ee62b39
penguin <penguin_liu@163.com>
2020-09-22 10:40:47 +0800

    diyi

---
+++ KCObjc/PHPerson.h
@@ -0,0 +1,18 @@
+//
+//  LGPerson.h
+//  KCObjc
+//
+//  Created by Cooci on 2020/7/24.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PHPerson : NSObject
+- (void)doFirst;
+- (void)doSecond;
+- (void)doThird;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ KCObjc/PHPerson.m
@@ -0,0 +1,22 @@
+//
+//  LGPerson.m
+//  KCObjc
+//
+//  Created by Cooci on 2020/7/24.
+//
+
+#import "PHPerson.h"
+
+@implementation PHPerson
+
+- (void)doFirst{
+    
+}
+- (void)doSecond{
+    
+}
+- (void)doThird{
+    
+}
+
+@end
